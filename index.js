1c247e6a73cf31e177f64fb86b24873efec977de
rezwanul24 <rezwanul.shajib@hotmail.com>
2023-01-30 10:36:31 +0600

    dot env setup

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
-const cors = require('cors')
-const port = process.env.PORT || 5000
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.wm2o919.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
