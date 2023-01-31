9894fd285ab94c6960c5ee22a801e0ba24bbe347
rezwanul24 <rezwanul.shajib@hotmail.com>
2023-01-31 12:03:06 +0600

    single service and all service

--- index.js
+++ index.js
@@ -54,7 +54,23 @@ async function run(){
       res.send(services);
   });
 
-  
+  // get all services 
+  app.get('/services', async (req, res) => {
+    const query = {}
+    const cursor = serviceCollection.find(query);
+    const services = await cursor.toArray();
+    res.send(services);
+});
+
+// get single service 
+app.get('/service/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const service = await serviceCollection.findOne(query);
+    res.send(service);
+});
+
+
   }
   
   finally{
