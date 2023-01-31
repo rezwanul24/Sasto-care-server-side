564d326c97f8ada8df238e5fc419c4149e81539c
rezwanul24 <rezwanul.shajib@hotmail.com>
2023-01-31 12:00:00 +0600

    login user added

--- index.js
+++ index.js
@@ -37,11 +37,12 @@ async function run(){
     const serviceCollection = client.db('sastohcare').collection('services');
     const reviewsCollection = client.db('sastohcare').collection('reviews');
 
-    app.post('/jwt', (req,res)=> {
+    // login user 
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-      res.send({token})
-    })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      res.send({ token })
+  })
 
     // add service 
     app.post('/service', async (req, res) => {
