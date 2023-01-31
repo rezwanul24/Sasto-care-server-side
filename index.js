638dac782069999a34c4b1cca8c998f6545c4ae1
rezwanul24 <rezwanul.shajib@hotmail.com>
2023-01-31 11:24:02 +0600

    jwt verify added

--- index.js
+++ index.js
@@ -15,6 +15,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+  function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+      return res.status(401).send({massage: 'unauthorize access'})
+    }
+
+    const token = authHeader.splite(' ')[1];
+    jwt.verif(token, process.env.ACCESS_TOKEN_SECRET, function(error, decoded){
+      if(error){
+        res.status(401).send({massage: 'forbeden access'})
+      }
+      req.decoded = decoded;
+      next()
+    })
+  }
+
 async function run(){
   try{
     const serviceCollection = client.db('sastohcare').collection('services');
